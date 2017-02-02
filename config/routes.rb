9b57a606ddcaf8678137ccca0845bb2f0430c064
Jim Padilla <jp_code2013@yahoo.com>
2017-02-02 14:34:04 -0800

    Passing Rspec

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Lists route
-  resources :lists
+  resources :lists do
+    resources :tasks
+  end
+
+  resources :tasks do
+    resources :subtasks
+  end
+
 end
