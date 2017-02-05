ac3d823cd74d4cc0d60ecd7c670905296a6da9f9
Jim Padilla <jp_code2013@yahoo.com>
2017-02-04 16:49:35 -0800

    Add edit/update functions to tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,36 @@ class TasksController < ApplicationController
     @list = List.find(params[:list_id])
     @task = @list.tasks.new
   end
+
+  def create
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.new(task_params)
+    if @task.save
+      redirect_to list_path(@task.list)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+                    # What does this argument do?
+      redirect_to list_path(@task.list)
+    else
+      render :edit
+    end
+  end
+
+private
+  def task_params
+    params.require(:task).permit(:description)
+  end
 end
